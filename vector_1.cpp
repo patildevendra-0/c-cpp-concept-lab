2400f5f636950a3cc940bd4c5f8b123355aa8988
patildevendra-0 <patildev2001@gmail.com>
2026-02-16 09:01:42 +0530

    1.3

--- vector_1.cpp
+++ vector_1.cpp
@@ -13,6 +13,26 @@ int main()
 
 
     cout<<"SIZE oF iVec : "<<iVec.size() <<"\n";
+    cout<<"CAPACITY oF iVec : "<<iVec.capacity() <<"\n";
+    cout<<"Is Empty iVec : "<<iVec.empty() <<"\n";
+
+    iVec.at(0);
+    iVec.front();
+    iVec.back();
+
+
+    iVec.insert(iVec.begin()+1,100);
+    iVec.erase(iVec.begin());
+
+    iVec.resize(4);
+    iVec.reserve(100);
+
+    for (int iCnt:iVec)
+    {
+        cout<<iCnt;
+    }
+
+
 
     return 0;
 }
