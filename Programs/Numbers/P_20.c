11e38b7d08af08fe78d5abc50ede944db3eb6515
patildevendra-0 <patildev2001@gmail.com>
2026-04-07 08:32:31 +0530

    2.3

--- Programs/Numbers/P_20.c
+++ Programs/Numbers/P_20.c
@@ -1,12 +1,32 @@
 // Product,Sum,Count
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    printf("SHREE GANESH...")
+    printf("SHREE GANESH...");
 
+    int iCount = 0;
+    int iSum = 0;
+    int iProduct = 1;
+    int iNo = 0;
+    int iDigit = 0;
 
+    printf("ENTER THE NUMBER  : ");
+    scanf("%d", &iNo);
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iSum = iSum + iDigit;
+        iProduct = iProduct * iDigit;
+        iCount++;
+        iNo = iNo / 10;
+    }
+
+    printf("PRODUCT : %d\n", iProduct);
+    printf("SUM : %d\n", iSum);
+    printf("COUNT : %d\n", iCount);
 
     return 0;
 }
